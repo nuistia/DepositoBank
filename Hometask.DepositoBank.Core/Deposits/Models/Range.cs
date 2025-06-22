86f7a9e3904f75c13d7d562a28cfe05e76c498bd
Anastasiia Atanasevych <nuistia@gmail.com>
2025-06-22 18:27:04 +0300

    Fix Contains() method for correct matching

--- Hometask.DepositoBank.Core/Deposits/Models/Range.cs
+++ Hometask.DepositoBank.Core/Deposits/Models/Range.cs
@@ -7,5 +7,5 @@ public class Range<T>(T start, T end) where T : INumber<T>
     public T Start { get; init; } = start;
     public T End { get; init; } = end;
 
-    public bool Contains(T value) => Start <= value && End < value;
+    public bool Contains(T value) => Start <= value && End > value;
 }
