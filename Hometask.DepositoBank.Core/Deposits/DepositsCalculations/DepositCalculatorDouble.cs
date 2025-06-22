2e6a1ebdd643949bc5e1a0dc2cdf675aa590ed1b
Anastasiia Atanasevych <nuistia@gmail.com>
2025-06-22 18:49:38 +0300

    Implement deposit calculation with double values

---
+++ Hometask.DepositoBank.Core/Deposits/DepositsCalculations/DepositCalculatorDouble.cs
@@ -0,0 +1,23 @@
+﻿using Hometask.DepositoBank.Core.Deposits.Models;
+using Hometask.DepositoBank.Core.Deposits.Models.DepositPercentages;
+
+namespace Hometask.DepositoBank.Core.Deposits.DepositsCalculations;
+
+public class DepositCalculatorDouble : IDepositCalculator<double>
+{
+    private const double PERCENT_COEFF = 0.01;
+
+    public double Deposit(double money, Bonuses bonus = Bonuses.No)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(money);
+
+        var rates = DepositPercentageDouble.Rates;
+        double result = 0;
+
+        var rateMatch = rates.Single(r => r.Key.Contains(money));
+
+        result = money + (money * ((int)rateMatch.Value * PERCENT_COEFF)) + (int)bonus;
+
+        return result;
+    }
+}
---
+++ Hometask.DepositoBank.Core/Deposits/DepositsCalculations/IDepositCalculator.cs
@@ -0,0 +1,9 @@
+﻿using Hometask.DepositoBank.Core.Deposits.Models;
+using System.Numerics;
+
+namespace Hometask.DepositoBank.Core.Deposits.DepositsCalculations;
+
+public interface IDepositCalculator<T> where T : INumber<T>
+{
+    T Deposit(T money, Bonuses bonus = Bonuses.No);
+}
